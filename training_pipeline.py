ebd90e4028800c8a191237a6d1f7ba9f2b7552c0
sumit kushwah <you@example.com>
2022-09-20 11:39:10 +0530

    checking shell

--- training_pipeline.py
+++ training_pipeline.py
@@ -44,7 +44,7 @@ def TrainingPipeline():
     data_extraction = SSHOperator(
         task_id='data_extraction',
         ssh_conn_id='ssh_default',
-        command='/home/ubuntu/miniconda3/condabin/conda init bash && \
+        command='echo $SHELL && \
             /home/ubuntu/miniconda3/condabin/conda activate fasttext_model_training && \
             cd /home/ubuntu/fasttext_model_training && \
             dvc update product-non-pharma.dvc',
