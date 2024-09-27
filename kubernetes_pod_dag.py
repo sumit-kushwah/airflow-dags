ae5db128d56af2bf092543c3f8ef911e152d442d
Sumit Kushwah <sumitkushwah1729@gmail.com>
2024-09-27 11:41:26 +0530

    fixed hello world dag

--- kubernetes_pod_dag.py
+++ kubernetes_pod_dag.py
@@ -1,6 +1,7 @@
 from airflow import DAG
-from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
 from datetime import datetime
+from airflow.providers.cncf.kubernetes.operators.pod import KubernetesPodOperator
+import airflow.providers.cncf.kubernetes as k8s
 
 # Default arguments
 default_args = {
@@ -19,21 +20,13 @@ with DAG(
 
     # Define the KubernetesPodOperator task
     run_custom_docker_task = KubernetesPodOperator(
-        namespace='default',  # Namespace where the pod will be created
+        namespace='airflow',  # Namespace where the pod will be created
         image='hello-world:latest',  # Replace with your custom Docker image
-        cmds=["python", "-c"],
         arguments=["print('Hello from custom Docker image!')"],
-        name="run-custom-docker-task",
-        task_id="run_custom_docker_task",
+        name="hello-world-task",
         in_cluster=True,  # If you're running the task within the Kubernetes cluster
         is_delete_operator_pod=True,  # Clean up the pod after task completes
         get_logs=True,  # Retrieve logs from the pod
-        resources={
-            "request_memory": "200Mi",  # Request 200MB of memory
-            "request_cpu": "500m",      # Request 0.5 CPU
-            "limit_memory": "200Mi",    # Limit memory usage to 200MB
-            "limit_cpu": "500m",        # Limit CPU usage to 0.5 CPU
-        },
     )
 
 # The task will run when the DAG is executed
